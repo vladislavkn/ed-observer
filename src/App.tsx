669b8a40da86b5720a2e06db2a03a72358d44e32
VladKnyshov <vknyshov@digitalskynetcorp.com>
2021-09-10 17:10:28 +0300

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import LoginPage from "./pages/LoginPage";
+import WeekdaysPage from "./pages/WeekdaysPage";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navigation />
+
+        <Switch>
+          <Route path="/login" component={LoginPage} />
+          <Route path="/edit">
+            <Redirect to="/login" />
+          </Route>
+          <Route path="/" component={WeekdaysPage} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
