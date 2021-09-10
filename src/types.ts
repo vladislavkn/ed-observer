b549c428398308d62f926b714b02b5d87d32ed03
VladKnyshov <vknyshov@digitalskynetcorp.com>
2021-09-10 17:06:03 +0300

    add DTO types & update lesson types

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,42 @@
-import React from "react";
-import { GenericType } from "typescript";
-
 export type Lesson = {
   name: string;
   id: string;
   teacher?: string;
   adress?: string;
   homework?: string;
-  type: "lection" | "practice";
+  type: "lection" | "practice" | "lab";
+  time: string;
 };
 
 export type Weekday = {
   isOnline?: boolean;
   date: Date;
   lessons: Lesson[];
-  id: string;
+  id: number | string;
+};
+
+export type ScheduleLessonDTO = {
+  name: string;
+  teachers: string[];
+  rooms: string[];
+  time_start: string;
+  time_end: string;
+  types: string;
+  weeks: number[];
+};
+
+export type ScheduleDayDTO = {
+  lessons: ScheduleLessonDTO[][];
+};
+
+export type ScheduleDTO = {
+  group: string;
+  schedule: {
+    1: ScheduleDayDTO;
+    2: ScheduleDayDTO;
+    3: ScheduleDayDTO;
+    4: ScheduleDayDTO;
+    5: ScheduleDayDTO;
+    6: ScheduleDayDTO;
+  };
 };
