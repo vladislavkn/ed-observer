b143d389c228dc8f6eb744dfd0daad3f07aa2c2e
VladKnyshov <vknyshov@digitalskynetcorp.com>
2021-09-12 14:36:00 +0300

    add WeekdaysPage

--- src/pages/WeekdaysPage.tsx
+++ src/pages/WeekdaysPage.tsx
@@ -1,35 +1,24 @@
-import { addDays, subDays } from "date-fns";
 import { useState } from "react";
 import weekdaysApi from "../api/weekdaysApi";
 import AppEmptyFallback from "../components/AppEmptyFallback";
 import AppLoad from "../components/AppLoad";
 import AppLoadingFallback from "../components/AppLoadingFallback";
+import EditPagePanel from "../components/EditPagePanel";
 import WeekdayCard from "../components/WeekdayCard";
+import WeekdaySelector from "../components/WeekdaySelector";
 import { Weekday } from "../types";
-import { formatDayAndMonth, getMonday } from "../utils";
+import { getMonday } from "../utils";
 
 const WeekdaysPage: React.FC = () => {
   const [currentDay, setCurrentDay] = useState(getMonday(new Date()));
 
-  const handleNext = () => setCurrentDay((prev) => addDays(prev, 7));
-  const handlePrev = () => setCurrentDay((prev) => subDays(prev, 7));
-
   return (
-    <div className="container pt-3 pb-2">
-      <div className="d-flex justify-content-between align-items-center mb-3">
-        <h6>
-          {formatDayAndMonth(currentDay)} -{" "}
-          {formatDayAndMonth(addDays(currentDay, 6))}
-        </h6>
-        <div className="btn-group btn-group-lg " role="group">
-          <button type="button" className="btn btn-light" onClick={handlePrev}>
-            <i className="bi bi-arrow-left"></i>
-          </button>
-          <button type="button" className="btn btn-light" onClick={handleNext}>
-            <i className="bi bi-arrow-right"></i>
-          </button>
-        </div>
-      </div>
+    <div className="container my-4">
+      <EditPagePanel />
+      <WeekdaySelector
+        value={currentDay}
+        onChange={(value) => setCurrentDay(value)}
+      />
       <div className="row">
         <AppLoad<Weekday[]>
           fetcher={() => weekdaysApi.getWeek(currentDay, "ИНБО-01-21")}
