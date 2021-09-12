3024bd96c0fa616e62c8f95f4693d1f29faf1e88
VladKnyshov <vknyshov@digitalskynetcorp.com>
2021-09-12 14:36:06 +0300

    add LoginPage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,26 @@
+import { FormEventHandler, useState } from "react";
+import { useHistory } from "react-router-dom";
+import authApi from "../api/authApi";
+
 const LoginPage: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+
+  const handleSubmit: FormEventHandler = async (e) => {
+    e.preventDefault();
+    authApi.login(email, password).then(() => history.push("/"));
+  };
+
   return (
-    <div className="container mt-3">
+    <div className="container my-4">
       <div className="row">
         <div className="col-12 col-md-6 offset-md-3 col-lg-4 offset-lg-4">
           <div className="card">
-            <form className="card-body">
+            <form className="card-body" onSubmit={handleSubmit}>
               <h5 className="card-title mb-3">
-                Введите пароль для редактрования
+                Представьтесь для редактирования
               </h5>
-
               <div className="mb-3">
                 <label htmlFor="email-field" className="form-label">
                   Почта
@@ -18,6 +30,10 @@ const LoginPage: React.FC = () => {
                   className="form-control"
                   id="email-field"
                   placeholder="Email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  name="email"
                 />
               </div>
               <div className="mb-3">
@@ -29,6 +45,10 @@ const LoginPage: React.FC = () => {
                   className="form-control"
                   id="password-field"
                   placeholder="Password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  name="password"
                 />
               </div>
               <button type="submit" className="btn btn-primary">
