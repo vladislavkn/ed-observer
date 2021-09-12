1974a068f43f4ee87926e00033c322bc550b7fdd
VladKnyshov <vknyshov@digitalskynetcorp.com>
2021-09-12 14:32:44 +0300

    add persistence to auth state

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
 import App from "./App";
+import { auth } from "./firebase";
+import { browserLocalPersistence, setPersistence } from "@firebase/auth";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+Promise.all([setPersistence(auth, browserLocalPersistence)]).then(() =>
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
 );
