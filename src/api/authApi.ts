c730dc29b3d899549359564a0acd2dc4ba2e26cb
VladKnyshov <vknyshov@digitalskynetcorp.com>
2021-09-14 23:32:34 +0300

    refactor auth api

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,20 +1,9 @@
 import { signInWithEmailAndPassword, signOut } from "firebase/auth";
-import { toast } from "react-toastify";
 import { auth } from "../firebase";
 
 class AuthApi {
   async login(email: string, password: string) {
-    return signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        toast.success(`Вы вошли в аккаунт как ${userCredential.user.email}`);
-        return userCredential.user;
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-
-        toast.error(`Ошибка ${errorCode}: ${errorMessage}`);
-      });
+    return signInWithEmailAndPassword(auth, email, password);
   }
 
   async logout() {
