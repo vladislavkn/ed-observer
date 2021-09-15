9951a6be0e84070d987dee47a3c9bbd3eda25590
VladKnyshov <vknyshov@digitalskynetcorp.com>
2021-09-15 14:35:21 +0300

    add commonScheduleApi

---
+++ src/api/commonScheduleAPI.ts
@@ -0,0 +1,80 @@
+import {
+  CommonSchedule,
+  CommonScheduleDay,
+  CommonScheduleDayLesson,
+  ScheduleDayDTO,
+  ScheduleDTO,
+  ScheduleLessonDTO,
+} from "../types";
+
+class CommonScheduleAPI {
+  public async fetchCommonSchedule(groupName: string): Promise<CommonSchedule> {
+    const scheduleDTO = await this.fetchScheduleDTO(groupName);
+    return this.convertScheduleDTOToCommonSchedule(scheduleDTO);
+  }
+
+  private fetchScheduleDTO(groupName: string): Promise<ScheduleDTO> {
+    return fetch(this.getAPIUrl(groupName)).then((res) => res.json());
+  }
+
+  private getAPIUrl(groupName: string) {
+    return (
+      process.env.REACT_APP_API_URL + `/schedule/${groupName}/full_schedule`
+    );
+  }
+
+  private convertScheduleDTOToCommonSchedule(
+    scheduleDTO: ScheduleDTO
+  ): CommonSchedule {
+    return {
+      even: this.getCommonScheduleWeek(scheduleDTO, 0),
+      odd: this.getCommonScheduleWeek(scheduleDTO, 1),
+    };
+  }
+
+  private getCommonScheduleWeek(
+    scheduleDTO: ScheduleDTO,
+    parity: number
+  ): CommonScheduleDay[] {
+    return Object.values(scheduleDTO.schedule).map((scheduleDayDTO) =>
+      this.getCommonScheduleDay(scheduleDayDTO, parity)
+    );
+  }
+
+  private getCommonScheduleDay(
+    scheduleDayDTO: ScheduleDayDTO,
+    parity: number
+  ): CommonScheduleDay {
+    return {
+      lessons: scheduleDayDTO.lessons.map((lessonVariants, index) =>
+        this.getCommonScheduleDayLessonByVariant(lessonVariants, index, parity)
+      ),
+    };
+  }
+
+  private getCommonScheduleDayLessonByVariant(
+    lessonVariants: ScheduleLessonDTO[],
+    order: number,
+    parity: number
+  ) {
+    const lesson = lessonVariants.filter(
+      (lesson) => lesson.weeks[0] % 2 === parity
+    )[0];
+
+    const lessonTypes: Record<
+      ScheduleLessonDTO["types"],
+      CommonScheduleDayLesson["type"]
+    > = { лк: "lection", пр: "practice", "лаб\nлаб": "lab" };
+
+    return {
+      name: lesson.name,
+      type: lessonTypes[lesson.types],
+      time: `${lesson.time_start}-${lesson.time_end}`,
+      teacher: lesson?.teachers.join(" "),
+      adress: lesson.rooms.join(", "),
+      order,
+    };
+  }
+}
+
+export default new CommonScheduleAPI();
