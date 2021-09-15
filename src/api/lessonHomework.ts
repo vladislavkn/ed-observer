e96a9a5e68f8be03141db4bec8240082485a5413
VladKnyshov <vknyshov@digitalskynetcorp.com>
2021-09-15 16:01:28 +0300

    add LessonHomework api class

---
+++ src/api/lessonHomework.ts
@@ -0,0 +1,39 @@
+import { doc, DocumentReference, getDoc, setDoc } from "firebase/firestore";
+import { auth, firestore } from "../firebase";
+import { Homework, ScheduleDayLesson } from "../types";
+
+export default class LessonHomework {
+  private static homeworkDocs: Record<ScheduleDayLesson["id"], LessonHomework> =
+    {};
+
+  constructor(public lessonId: string) {
+    if (LessonHomework.homeworkDocs[lessonId])
+      return LessonHomework.homeworkDocs[lessonId];
+  }
+
+  public async fetchHomework(): Promise<Homework> {
+    const doc = await getDoc(this.doc);
+    if (doc.exists()) return doc.data();
+    return { text: "", userId: auth.currentUser?.uid ?? "", isDraft: true };
+  }
+
+  public async setHomework(homework: Homework): Promise<void> {
+    if (homework.isDraft && homework.text.length === 0) return;
+    if (homework.isDraft) delete homework.isDraft;
+    if (!auth.currentUser)
+      throw new Error(
+        "Незарегестрированный пользователь не может сохранять домашние задания"
+      );
+
+    homework.userId = auth.currentUser.uid;
+    await setDoc(this.doc, homework);
+  }
+
+  private get doc() {
+    return doc(
+      firestore,
+      "homeworks",
+      this.lessonId
+    ) as DocumentReference<Homework>;
+  }
+}
