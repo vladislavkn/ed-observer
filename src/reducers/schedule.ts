c421b8ff939e317fc117f78e92200b16ceb37d9b
VladKnyshov <vknyshov@digitalskynetcorp.com>
2021-09-15 14:34:38 +0300

    add schedule reducers

--- src/reducers/schedule.ts
+++ src/reducers/schedule.ts
@@ -1,5 +1,5 @@
-import { Action } from "redux";
-import { CommonSchedule, Schedule } from "../types";
+import { ScheduleActions } from "../actions/schedule";
+import { ActionWithPayload, CommonSchedule, Schedule } from "../types";
 
 type ScheduleState = {
   commonSchedule: CommonSchedule | null;
@@ -17,9 +17,30 @@ const initialState: ScheduleState = {
 
 export default function scheduleReducer(
   state = initialState,
-  action: Action
+  action: ActionWithPayload<
+    ScheduleActions,
+    CommonSchedule | Schedule | Error | undefined
+  >
 ): ScheduleState {
   switch (action.type) {
+    case ScheduleActions.SET_COMMON_SCHEDULE_SUCCESS:
+      return {
+        ...state,
+        commonSchedule: action.payload as CommonSchedule,
+        error: null,
+        loading: false,
+      };
+    case ScheduleActions.SET_COMMON_SCHEDULE_FAILTURE:
+      return {
+        ...state,
+        commonSchedule: null,
+        error: action.payload as Error,
+        loading: false,
+      };
+    case ScheduleActions.START_COMMON_SCHEDULE_LOAD:
+      return { ...state, error: null, loading: true };
+    case ScheduleActions.SET_SCHEDULE:
+      return { ...state, schedule: action.payload as Schedule };
     default:
       return state;
   }
