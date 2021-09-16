8a758fb7a9704e121367d9b0350a81bd40854534
VladKnyshov <vknyshov@digitalskynetcorp.com>
2021-09-16 20:35:12 +0300

    update actions

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -1,11 +1,43 @@
 import { User } from "firebase/auth";
+import { toast } from "react-toastify";
+import { AnyAction } from "redux";
+import { ThunkAction } from "redux-thunk";
+import authApi from "../api/authApi";
+import { RootState } from "../store";
 
 export enum UserActions {
-  SET_USER_SUCCESS,
-  SET_USER_FAILTURE,
-  START_USER_LOAD,
-  REMOVE_USER,
+  SET_USER_SUCCESS = "SET_USER_SUCCESS",
+  SET_USER_FAILTURE = "SET_USER_FAILTURE",
+  START_USER_LOAD = "START_USER_LOAD",
+  REMOVE_USER = "REMOVE_USER",
 }
+
+export const login =
+  (
+    email: string,
+    password: string,
+    callback: () => void
+  ): ThunkAction<void, RootState, Promise<void>, AnyAction> =>
+  (dispatch) => {
+    authApi
+      .login(email, password)
+      .then((userData) => {
+        if (userData.user) {
+          toast.success(`Вы вошли в аккаунт как ${userData.user.email}`);
+          callback();
+        } else toast.warn("Неверные данные");
+      })
+      .catch((error) => {
+        toast.error(`Ошибка: ${error.message}`);
+        console.error(error);
+      });
+  };
+
+export const logout =
+  (): ThunkAction<void, RootState, Promise<void>, AnyAction> => async () => {
+    await authApi.logout();
+    toast.success(`Вы успешно вышли из аккаунта`);
+  };
 
 export const setUserSuccess = (user: User) => ({
   type: UserActions.SET_USER_SUCCESS,
