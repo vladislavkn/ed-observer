7ee0d9a98e12071f0742b7ea10835e272d999e2d
VladKnyshov <vknyshov@digitalskynetcorp.com>
2021-09-16 20:42:19 +0300

    update schedule selectors

--- src/hooks/schedule.ts
+++ src/hooks/schedule.ts
@@ -3,3 +3,9 @@ import { RootState } from "../store";
 
 export const useScheduleState = () =>
   useSelector<RootState, RootState["schedule"]>((state) => state.schedule);
+
+export const useCurrentMonday = () =>
+  useSelector<RootState, Date | null>(
+    (state) =>
+      (state.schedule.schedule && state.schedule.schedule[0].date) ?? null
+  );
